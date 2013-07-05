271f33285ea10c6777333aa851cdfdeab794e50b
Ubuntu <ubuntu@ip-172-31-45-144.us-west-2.compute.internal>
2013-07-05 09:12:37 +0000

    Debugg web.js(3): missing );

--- web.js
+++ web.js
@@ -6,9 +6,10 @@ app.get('/', function(request, response) {
   var message = sendMessage();
   response.send(message);
 
-}
+});
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
