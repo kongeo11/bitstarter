1c8f3e75201e5afa248e08ab41353293ed6b3b55
Ubuntu <ubuntu@ip-172-31-22-142.us-west-2.compute.internal>
2013-07-16 16:44:57 +0000

    Update index.html with css style

--- web.js
+++ web.js
@@ -8,7 +8,8 @@ app.get('/', function(request, response) {
 
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080 //we had 5000;
 
 app.listen(port, function() {
   console.log("Listening on " + port);
@@ -27,3 +28,4 @@ function sendMessage() {
 
 
 }
+
